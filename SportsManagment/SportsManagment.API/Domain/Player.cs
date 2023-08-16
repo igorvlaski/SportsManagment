a22f8af1c7a7e9e33562072e233c2f5e7dd355d3
Igor <vlaski.igor@gmail.com>
2023-08-16 14:20:43 +0200

    Added new info to player entity

--- SportsManagment/SportsManagment.API/Domain/Player.cs
+++ SportsManagment/SportsManagment.API/Domain/Player.cs
@@ -6,6 +6,11 @@ public class Player
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public DateOnly DateOfBirth { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? ParentName { get; set; }
+    public string? ParentPhoneNumber { get; set; }
     public bool IsMonthlyFeePaid { get; set; }
     public bool IsYearlyFeePaid { get; set; }
     public List<TrainingAttendance> TrainingAttendances { get; set; } = new List<TrainingAttendance>();
---
+++ SportsManagment/SportsManagment.API/Migrations/20230816121506_AddNewPlayerInfo.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsManagment.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewPlayerInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Players",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Players",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ParentName",
+                table: "Players",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ParentPhoneNumber",
+                table: "Players",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Players",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "ParentName",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "ParentPhoneNumber",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Players");
+        }
+    }
+}
