bfad642cbb9ee4e342a8b42f4d6e3e6ead47140c
Igor <vlaski.igor@gmail.com>
2024-01-08 12:23:52 +0100

    Change double Amount to decimal

--- SportsManagment/SportsManagment.Shared/Domain/PaymentInformation.cs
+++ SportsManagment/SportsManagment.Shared/Domain/PaymentInformation.cs
@@ -4,7 +4,7 @@ public class PaymentInformation
 {
     public Guid Id { get; set; }
     public DateOnly DateOfPayment { get; set; }
-    public double Amount { get; set; }
+    public decimal Amount { get; set; }
     public string? Description { get; set; }
     public Guid PlayerId { get; set; }
     public TypeOfPayment typeOfPayment { get; set; }
